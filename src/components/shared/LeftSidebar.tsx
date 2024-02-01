80ebadd8996f495a3be0c0cb0dccb03ff45e4644
richardbrrito <richardbrrito116@gmail.com>
2024-02-01 12:46:22 -0500

    Fixed logout button, now logs out correctly.

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -2,20 +2,28 @@ import { Link, NavLink, useNavigate, useLocation } from "react-router-dom";
 import { Button } from "../ui/button";
 import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
 import { useEffect } from "react";
-import { useUserContext } from "@/context/AuthContext";
+import { INITIAL_USER, useUserContext } from "@/context/AuthContext";
 import { sidebarLinks } from "@/constants";
 import { INavLink } from "@/types";
+import { Loader } from "lucide-react";
 
 const LeftSidebar = () => {
-  const { pathname } = useLocation();
-
-  const { mutate: signOut, isSuccess } = useSignOutAccount();
   const navigate = useNavigate();
-  const { user } = useUserContext();
+  const { pathname } = useLocation();
+  const { user, setUser, setIsAuthenticated, isLoading } = useUserContext();
 
-  useEffect(() => {
-    if (isSuccess) navigate(0);
-  }, [isSuccess]);
+  const { mutate: signOut } = useSignOutAccount();
+
+  const handleSignOut = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    signOut();
+    setIsAuthenticated(false);
+    setUser(INITIAL_USER);
+    navigate("/sign-in");
+  };
+
   return (
     <nav className="leftsidebar">
       <div className="flex flex-col gap-11">
@@ -28,32 +36,37 @@ const LeftSidebar = () => {
           />
         </Link>
 
-        <Link to={"/profile/${user.id}"} className="flex gap-3 items-center">
-          <img
-            src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
-            alt="profile"
-            className="h-14 w-14 rounded-full"
-          ></img>
-          <div className="flex flex-col">
-            <p className="body-bold">{user.name}</p>
-            <p className="small-regular text-light-3">@{user.username}</p>
+        {isLoading || !user.email ? (
+          <div className="h-14">
+            <Loader />
           </div>
-        </Link>
+        ) : (
+          <Link to={`/profile/${user.id}`} className="flex gap-3 items-center">
+            <img
+              src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+              alt="profile"
+              className="h-14 w-14 rounded-full"
+            />
+            <div className="flex flex-col">
+              <p className="body-bold">{user.name}</p>
+              <p className="small-regular text-light-3">@{user.username}</p>
+            </div>
+          </Link>
+        )}
 
         <ul className="flex flex-col gap-6">
           {sidebarLinks.map((link: INavLink) => {
             const isActive = pathname === link.route;
+
             return (
               <li
                 key={link.label}
                 className={`leftsidebar-link group ${
                   isActive && "bg-primary-500"
-                }`}
-              >
+                }`}>
                 <NavLink
                   to={link.route}
-                  className="flex gap-4 items-center p-4"
-                >
+                  className="flex gap-4 items-center p-4">
                   <img
                     src={link.imgURL}
                     alt={link.label}
@@ -68,12 +81,12 @@ const LeftSidebar = () => {
           })}
         </ul>
       </div>
+
       <Button
         variant="ghost"
         className="shad-button_ghost"
-        onClick={() => signOut}
-      >
-        <img src="/assets/icons/logout.svg" alt="logout"></img>
+        onClick={(e) => handleSignOut(e)}>
+        <img src="/assets/icons/logout.svg" alt="logout" />
         <p className="small-medium lg:base-medium">Logout</p>
       </Button>
     </nav>
